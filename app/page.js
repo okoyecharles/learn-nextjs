6788c400fbfe300a04d12f6357b777532ead9f7e
Charles <okoyecharles509@gmail.com>
2023-08-23 22:09:08 +0100

    Write out Component structure for Blog app

--- app/page.js
+++ app/page.js
@@ -2,6 +2,21 @@ export default function Home() {
   // Blog app todo:
   // Home page renders list of posts and search bar to search for posts
   // Post page displays post with navigation buttons (prev and next)
-  
+
+  // Blog app component structure:
+  // app
+  // - search bar
+  // - list of posts
+  // - - post
+  // - - - post index
+  // - - - post title
+  // posts/[id]
+  // - layout
+  // - - previous and next buttons
+  // - post info
+  // - - post title
+  // - - post date
+  // - - post content
+
   return <div>Hello World</div>
 }
